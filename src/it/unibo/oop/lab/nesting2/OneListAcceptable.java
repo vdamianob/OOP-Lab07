4de951d29fe57c8816185aacc488c6302dff54e3
Vittorio Damiano Brasini <vittorio.brasini@studio.unibo.it>
2021-11-09 00:16:02 +0100

    started ex4 (nesting2)

---
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -0,0 +1,13 @@
+package it.unibo.oop.lab.nesting2;
+
+import java.util.List;
+
+public class OneListAcceptable<T> implements Acceptable<List> {
+
+	@Override
+	public Acceptor<List> acceptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
