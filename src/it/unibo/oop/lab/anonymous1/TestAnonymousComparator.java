6a0747e6b38eba5164750c8d1a476726d485c875
Vittorio Damiano Brasini <vittorio.brasini@studio.unibo.it>
2021-11-08 12:33:11 +0100

    Finished ex1 (anonymous1) and started ex2 (nested1)

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,9 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -23,7 +26,7 @@ public final class TestAnonymousComparator {
 
     private TestAnonymousComparator() { }
 
-    /** asd
+    /**
      * Check whether the users in result list are in the same order as those in
      * expected.
      * 
@@ -74,7 +77,12 @@ public final class TestAnonymousComparator {
          * 
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
-        // TODO
+        final var comp = new Comparator<User>() {
+        	public int compare(User a, User b) {
+        		return Integer.compare(a.getAge(), b.getAge());
+        	}
+        };
+        Collections.sort(denzelUsers, comp);
         /*
          * expected Result
          */
@@ -105,7 +113,7 @@ public final class TestAnonymousComparator {
          * NOTE: in order to sort a list think about a method of the utility
          * class Collections
          */
-        // TODO
+        Collections.sort(rossiUsers, comp.reversed());
         /*
          * expected Result
          */
