bf93112ee3da55720bec43445c6e0a01575421d4
Vittorio Damiano Brasini <vittorio.brasini@studio.unibo.it>
2021-11-09 16:35:59 +0100

    finished last exercise (nesting2) | NOT TOO UNDERSTOOD

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,13 +1,50 @@
 package it.unibo.oop.lab.nesting2;
 
+import java.util.Iterator;
 import java.util.List;
 
-public class OneListAcceptable<T> implements Acceptable<List> {
+public class OneListAcceptable<T> implements Acceptable<T> {
+	
+	private List<T> list;	
+	
+	public OneListAcceptable(List<T> list) {
+		//super();
+		this.list = list;
+	}
 
 	@Override
-	public Acceptor<List> acceptor() {
-		// TODO Auto-generated method stub
-		return null;
+	public Acceptor<T> acceptor() {
+		final Iterator<T> iterator = list.iterator();
+        return new Acceptor<T>() {
+
+            /**
+             * @param newElement
+             *            the new element to be accepted
+             */
+            @Override
+            public void accept(final T newElement) throws Acceptor.ElementNotAcceptedException {
+                try {
+                    if (newElement.equals(iterator.next())) {
+                        return;
+                    }
+                } catch (Exception e) {
+                    System.out.println("no more element to be evaluated");
+                }
+                throw new Acceptor.ElementNotAcceptedException(newElement);
+            }
+
+            @Override
+            public void end() throws Acceptor.EndNotAcceptedException {
+                try {
+                    if (!iterator.hasNext()) {
+                        return;
+                    }
+                } catch (Exception e) {
+                    System.out.println("2" + e.getMessage());
+                }
+                throw new Acceptor.EndNotAcceptedException();
+            }
+        };
 	}
 
 }
